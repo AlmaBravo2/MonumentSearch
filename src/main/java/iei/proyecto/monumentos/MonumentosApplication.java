f183ba35af8dcd6b498738b58145eb95d3e10ac7
alexiserte <pelechan@embl.de>
2024-11-13 14:56:38 +0100

    Proyecto vacio de SpringBoot

---
+++ src/main/java/iei/proyecto/monumentos/MonumentosApplication.java
@@ -0,0 +1,13 @@
+package iei.proyecto.monumentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonumentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonumentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iei/proyecto/monumentos/MonumentosApplicationTests.java
@@ -0,0 +1,13 @@
+package iei.proyecto.monumentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonumentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

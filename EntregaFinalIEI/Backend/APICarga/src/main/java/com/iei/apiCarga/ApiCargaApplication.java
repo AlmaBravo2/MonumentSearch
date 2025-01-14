3184ede5a9de839743ba68c465f50ea770862abf
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2025-01-14 20:34:18 +0100

    Proyecto estructurado sin programar

---
+++ EntregaFinalIEI/Backend/APICarga/src/main/java/com/iei/apiCarga/ApiCargaApplication.java
@@ -0,0 +1,13 @@
+package com.iei.apiCarga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCargaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCargaApplication.class, args);
+	}
+
+}

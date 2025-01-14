e7bb150ca9632d684b9d99dac410ddd8a06f8c28
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2025-01-14 20:27:38 +0100

    Proyecto estructurado sin programar

---
+++ EntregaFinalIEI/Backend/APIBusqueda/src/main/java/com/iei/apiBusqueda/ApiBusquedaApplication.java
@@ -0,0 +1,13 @@
+package com.iei.apiBusqueda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBusquedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBusquedaApplication.class, args);
+	}
+
+}
---
+++ EntregaFinalIEI/Backend/APIBusqueda/src/test/java/com/iei/apiBusqueda/ApiBusquedaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iei.apiBusqueda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBusquedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

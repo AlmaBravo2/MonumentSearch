411f0b988d5406a3376fc7f31c47ad810be72adc
JeroAltur <160540354+JeroAltur@users.noreply.github.com>
2025-01-15 20:44:53 +0100

    tabla

--- EntregaFinalIEI/Frontend/PaginaBusqueda/script/script.js
+++ EntregaFinalIEI/Frontend/PaginaBusqueda/script/script.js
@@ -11,6 +11,8 @@ const circulo = L.circle([4.613573, -74.063889], {
 circulo.bindPopup("Programación en SIG")*/
 
 const monumentos = []
+const tabla = document.getElementById("table")
+
 for (const monumento of monumentos){
     var marcador = L.marker([monumento.longitud, monumento.latitud]).addTo(mapa)
     marcador.bindPopup(monumento.nombre + "/n/n" +
@@ -18,7 +20,38 @@ for (const monumento of monumentos){
         "Tipo: " + monumento.tipo + "/n/n" +
         monumento.descripcion + "/n/n" +
         monumento.longitud + " " + monumento.latitud)
+    const nuevaFila = document.createElement("tr")
+    const celdaNombre = document.createElement("td")
+    const celdaTipo = document.createElement("td")
+    const celdaDireccion = document.createElement("td")
+    const celdaLocalidad = document.createElement("td")
+    const celdaCodPostal = document.createElement("td")
+    const celdaProvincia = document.createElement("td")
+    const celdaDescripcion = document.createElement("td")
+    const celdaLongitud = document.createElement("td")
+    const celdaLatitud = document.createElement("td")
+    celdaNombre.textContent = monumento.nombre
+    celdaTipo.textContent = monumento.tipo
+    celdaDireccion.textContent = monumento.direccion
+    celdaLocalidad.textContent = monumento.localidad
+    celdaCodPostal.textContent = monumento.codigo_postal
+    celdaProvincia.textContent = monumento.provincia
+    celdaDescripcion.textContent = monumento.descripcion
+    celdaLongitud.textContent = monumento.longitud
+    celdaLatitud.textContent = monumento.latitud
+    nuevaFila.appendChild(celdaNombre)
+    nuevaFila.appendChild(celdaTipo)
+    nuevaFila.appendChild(celdaDireccion)
+    nuevaFila.appendChild(celdaLocalidad)
+    nuevaFila.appendChild(celdaCodPostal)
+    nuevaFila.appendChild(celdaDescripcion)
+    nuevaFila.appendChild(celdaLongitud)
+    nuevaFila.appendChild(celdaLatitud)
+
+    const cuerpoTabla = tabla.querySelector("tbody")
+    cuerpoTabla.appendChild(nuevaFila)
 }
+
 
 function clicSobreMapa(evento){
     alert("Diste clic en el punto con coordenadas latitud: " + evento.latlng.lat + " y longitud: " + evento.latlng.lng)

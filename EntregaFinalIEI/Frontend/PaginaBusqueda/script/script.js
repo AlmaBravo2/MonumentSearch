0cbeedf4219bd09e27e175ee1a850387bb3f2cd2
JeroAltur <160540354+JeroAltur@users.noreply.github.com>
2025-01-15 20:19:54 +0100

    for monuments

--- EntregaFinalIEI/Frontend/PaginaBusqueda/script/script.js
+++ EntregaFinalIEI/Frontend/PaginaBusqueda/script/script.js
@@ -10,6 +10,16 @@ const circulo = L.circle([4.613573, -74.063889], {
 }).addTo(mapa)
 circulo.bindPopup("Programación en SIG")*/
 
+const monumentos = []
+for (const monumento of monumentos){
+    var marcador = L.marker([monumento.longitud, monumento.latitud]).addTo(mapa)
+    marcador.bindPopup(monumento.nombre + "/n/n" +
+        monumento.direccion + "/n/n" +
+        "Tipo: " + monumento.tipo + "/n/n" +
+        monumento.descripcion + "/n/n" +
+        monumento.longitud + " " + monumento.latitud)
+}
+
 function clicSobreMapa(evento){
     alert("Diste clic en el punto con coordenadas latitud: " + evento.latlng.lat + " y longitud: " + evento.latlng.lng)
 }
@@ -18,4 +28,5 @@ mapa.on("click", clicSobreMapa);
 
 console.log(mapa)
 console.log(marcador)
-console.log(circulo)
+//console.log(circulo)
+

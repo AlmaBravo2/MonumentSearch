d4157571272ddb0719a716f2c0b3356e44cb1a4d
JeroAltur <160540354+JeroAltur@users.noreply.github.com>
2025-01-16 23:15:43 +0100

    AYUDA!!!!!

---
+++ EntregaFinalIEI/Backend/APICarga/src/main/java/com/iei/apiCarga/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.iei.apiCarga.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite todos los endpoints
+                        .allowedOrigins("*") // Permite solicitudes desde cualquier origen
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Permite cualquier cabecera
+                        .allowCredentials(false); // Deshabilita las credenciales si no son necesarias
+            }
+        };
+    }
+}

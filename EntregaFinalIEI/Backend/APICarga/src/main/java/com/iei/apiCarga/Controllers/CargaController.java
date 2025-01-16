307e637f95d098ca92c7b58d7ad31027e6a54994
Alejandro Iserte <alexiserte@gmail.com>
2025-01-16 18:20:44 +0100

    Swagger Carga

--- EntregaFinalIEI/Backend/APICarga/src/main/java/com/iei/apiCarga/Controllers/CargaController.java
+++ EntregaFinalIEI/Backend/APICarga/src/main/java/com/iei/apiCarga/Controllers/CargaController.java
@@ -7,6 +7,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Controller para la gestión de la carga de datos de monumentos en la base de datos.
+ * Proporciona endpoints para cargar datos selectivamente o completamente,
+ * así como para vaciar la base de datos.
+ */
 @RestController
 @CrossOrigin(origins = "*")
 @Tag(name = "Cargar", description = "Endpoints para cargar los monumentos de los wrappers en la base de datos")
@@ -15,6 +20,16 @@ public class CargaController {
     @Autowired
     private CargaService cargaService;
 
+    /**
+     * Endpoint para cargar datos en la base de datos.
+     * Este endpoint permite cargar datos de monumentos en la base de datos de forma selectiva o completa.
+     *
+     * @param todos Indica si se deben cargar todos los datos disponibles.
+     * @param cv Indica si se deben cargar datos específicos de la Comunidad Valenciana.
+     * @param eus Indica si se deben cargar datos específicos del País Vasco (Euskadi).
+     * @param cyl Indica si se deben cargar datos específicos de Castilla y León.
+     * @return Mensaje indicando el estado de la operación de carga.
+     */
     @Operation(
             summary = "Cargar datos en la base de datos",
             description = "Permite cargar datos de monumentos en la base de datos de forma selectiva o completa."
@@ -36,6 +51,10 @@ public class CargaController {
         return cargaService.cargarDatos(todos, cv, eus, cyl);
     }
 
+    /**
+     * Endpoint para eliminar todos los datos de la base de datos.
+     * Este endpoint elimina todos los registros de la base de datos relacionados con los monumentos cargados.
+     */
     @Operation(
             summary = "Eliminar todos los datos de la base de datos",
             description = "Permite eliminar todos los datos de la base de datos."

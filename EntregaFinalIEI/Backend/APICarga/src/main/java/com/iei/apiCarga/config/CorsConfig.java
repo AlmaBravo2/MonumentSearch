fa5655244a510027086412daafd7c1f031a24ca3
Alejandro Iserte <alexiserte@gmail.com>
2025-01-16 18:56:09 +0100

    se jodio la vaina

--- EntregaFinalIEI/Backend/APICarga/src/main/java/com/iei/apiCarga/config/CorsConfig.java
+++ EntregaFinalIEI/Backend/APICarga/src/main/java/com/iei/apiCarga/config/CorsConfig.java
@@ -1,3 +1,5 @@
+package com.iei.apiCarga.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;

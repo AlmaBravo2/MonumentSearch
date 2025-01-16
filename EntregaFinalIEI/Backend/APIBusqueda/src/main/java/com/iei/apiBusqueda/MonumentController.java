fa3466f646432278279ecd11aca363a2749eec82
JeroAltur <160540354+JeroAltur@users.noreply.github.com>
2025-01-16 15:49:01 +0100

    Swagger busqueda

--- EntregaFinalIEI/Backend/APIBusqueda/src/main/java/com/iei/apiBusqueda/MonumentController.java
+++ EntregaFinalIEI/Backend/APIBusqueda/src/main/java/com/iei/apiBusqueda/MonumentController.java
@@ -1,6 +1,9 @@
 package com.iei.apiBusqueda;
 
 import com.iei.apiBusqueda.MonumentoDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,14 +12,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/monumentos")
 @CrossOrigin(origins = "*")
+@Tag(name = "Monumentos", description = "Endpoints para buscar monumentos")
 public class MonumentController {
     @Autowired
     private BusquedaService busquedaService;
+
+    @Operation(
+            summary = "Buscar monumentos",
+            description = "Obtiene una lista de monumentos filtrados por localidad, código postal, provincia y tipo."
+    )
     @GetMapping("/")
-    public List<MonumentoDTO> getMonumentos(@RequestParam (name="localidad", defaultValue = "null") String localidad, @RequestParam (name="codigoPostal", defaultValue = "null") String codPostal,
-                                            @RequestParam (name="provincia", defaultValue = "null") String provincia, @RequestParam (name="tipo", defaultValue = "null") String tipo)
-    {
-        return busquedaService.monumentSearch(localidad,codPostal,provincia,tipo);
+    public List<MonumentoDTO> getMonumentos(
+            @Parameter(description = "Nombre de la localidad", example = "Valencia")
+            @RequestParam(name = "localidad", defaultValue = "null") String localidad,
+
+            @Parameter(description = "Código postal de la localidad", example = "46760")
+            @RequestParam(name = "codigoPostal", defaultValue = "null") String codPostal,
+
+            @Parameter(description = "Nombre de la provincia", example = "Valencia")
+            @RequestParam(name = "provincia", defaultValue = "null") String provincia,
+
+            @Parameter(description = "Tipo de monumento", example = "Histórico")
+            @RequestParam(name = "tipo", defaultValue = "null") String tipo
+    ) {
+        return busquedaService.monumentSearch(localidad, codPostal, provincia, tipo);
     }
 
 

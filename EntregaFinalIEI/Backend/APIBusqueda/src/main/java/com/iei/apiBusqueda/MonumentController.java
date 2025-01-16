0207632525976b22b231c9b795153d4c738c3bf6
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2025-01-16 13:08:25 +0100

    Cross origins APIBusqueda

--- EntregaFinalIEI/Backend/APIBusqueda/src/main/java/com/iei/apiBusqueda/MonumentController.java
+++ EntregaFinalIEI/Backend/APIBusqueda/src/main/java/com/iei/apiBusqueda/MonumentController.java
@@ -2,15 +2,13 @@ package com.iei.apiBusqueda;
 
 import com.iei.apiBusqueda.MonumentoDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/monumentos")
+@CrossOrigin(origins = "http://localhost:3000")
 public class MonumentController {
     @Autowired
     private BusquedaService busquedaService;
---
+++ EntregaFinalIEI/Backend/ExtractorEuskadi/src/main/java/com/wrapper/Euskadi/Service/EuskadiService.java
@@ -0,0 +1,2 @@
+package com.wrapper.Euskadi.Service;public class EuskadiService {
+}

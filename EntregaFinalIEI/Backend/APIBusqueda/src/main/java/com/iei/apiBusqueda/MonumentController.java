d276f567874098fe9e9a61192e76b36fd907d865
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2025-01-16 14:02:30 +0100

    cambio menor

--- EntregaFinalIEI/Backend/APIBusqueda/src/main/java/com/iei/apiBusqueda/MonumentController.java
+++ EntregaFinalIEI/Backend/APIBusqueda/src/main/java/com/iei/apiBusqueda/MonumentController.java
@@ -13,7 +13,7 @@ public class MonumentController {
     @Autowired
     private BusquedaService busquedaService;
     @GetMapping("/")
-    public List<MonumentoDTO> getMonumentos(@RequestParam (name="localidad", defaultValue = "null") String localidad, @RequestParam (name="cod postal", defaultValue = "null") String codPostal,
+    public List<MonumentoDTO> getMonumentos(@RequestParam (name="localidad", defaultValue = "null") String localidad, @RequestParam (name="codigoPostal", defaultValue = "null") String codPostal,
                                             @RequestParam (name="provincia", defaultValue = "null") String provincia, @RequestParam (name="tipo", defaultValue = "null") String tipo)
     {
         return busquedaService.monumentSearch(localidad,codPostal,provincia,tipo);

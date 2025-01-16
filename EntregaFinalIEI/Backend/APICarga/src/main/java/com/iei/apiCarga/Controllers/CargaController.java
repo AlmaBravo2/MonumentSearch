47abf3b2d274bfa68b6f9e5e51e821572d645948
Alejandro Iserte Fabuel <aisefab@upv.edu.es>
2025-01-16 13:49:17 +0100

    Cross

--- EntregaFinalIEI/Backend/APICarga/src/main/java/com/iei/apiCarga/Controllers/CargaController.java
+++ EntregaFinalIEI/Backend/APICarga/src/main/java/com/iei/apiCarga/Controllers/CargaController.java
@@ -2,11 +2,13 @@ package com.iei.apiCarga.Controllers;
 
 import com.iei.apiCarga.Services.CargaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class CargaController {
 
     @Autowired

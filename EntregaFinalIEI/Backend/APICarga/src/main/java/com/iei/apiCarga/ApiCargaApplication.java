8e99a460e2c6d9c4497be7f3826a4909f6f0aa65
Alejandro Iserte <alexiserte@gmail.com>
2025-01-16 18:57:05 +0100

    se jodio la vaina

--- EntregaFinalIEI/Backend/APICarga/src/main/java/com/iei/apiCarga/ApiCargaApplication.java
+++ EntregaFinalIEI/Backend/APICarga/src/main/java/com/iei/apiCarga/ApiCargaApplication.java
@@ -15,14 +15,4 @@ public class ApiCargaApplication {
 		SpringApplication.run(ApiCargaApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
-			}
-		};
-	}
-
 }
